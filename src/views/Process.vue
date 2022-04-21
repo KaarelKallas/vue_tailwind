d0d14e2b945306951951f0dfbb0ee9299b22064b
Kaarel Kallas <kaarel.kallas@ametikool.ee>
2022-04-21 14:50:34 +0300

    made news into components

--- src/views/Process.vue
+++ src/views/Process.vue
@@ -1,12 +1,14 @@
-
 <script setup>
-import axios from 'axios'
+    import NewsItem from "../components/NewsItem.vue";
+    import axios from 'axios'
 import { reactive, watch } from '@vue/runtime-core';
+
 
 const state = reactive({
     data: {},
     game: '730'
  })
+
 
 const fetchSteamData = (appID) => {
     axios.get('https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=' + appID + '&count=15&maxlength=10000&format=json')
@@ -20,26 +22,23 @@ watch(() => state.game, newValue => {
         fetchSteamData(newValue)
     }
 })
+
 </script>
 
 <template>
     <h1 class="text-center text-5xl mb-24">NEWS</h1>
-
-    <select id="game" v-model="state.game">
+    <div class="border mx-auto w-52 rounded-md mb-6 px-1 py-1">
+        <span>Choose a game: </span>
+        <select id="game" class="bg-sky-100 rounded-md" v-model="state.game">
         <option value="730">CS:GO</option>
         <option value="252490">Rust</option>
         <option value="578080">PUBG</option>
-    </select>
-    <div v-if="state.data.appnews?.newsitems">
-        <div
-            class="mb-12 w-[90%] md:w-[60%] mx-auto border-[1px] rounded-lg"
-            v-for="(item, index) in state.data.appnews?.newsitems"
-            :key="index"
-        >
-            <div class="pb-4 mt-2 text-center">
-                <span class="font-bold text-xl">{{ item.title }}</span>
-            </div>
-            <div class="mx-4 mb-4" v-html="item.contents"></div>
-        </div>
+        </select>
     </div>
+    <div>
+         <NewsItem v-for="(item, index) in state.data.appnews?.newsitems" :key="index" :data="item"/>
+    </div>
+
+
+
 </template>
